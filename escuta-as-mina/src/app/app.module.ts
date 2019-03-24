c1e8129b2c67c24e097e36cd8341c354f4eeb95e
Samantha Marques <sammy.marques@outlook.com>
2019-03-24 00:16:53 -0300

    Instala dependências

---
+++ escuta-as-mina/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FeedComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
