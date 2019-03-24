14da0f4d6ca9137a72518aab9920ffea05001a1d
Samantha Marques <sammy.marques@outlook.com>
2019-03-24 04:08:11 -0300

    Chamada no front

--- escuta-as-mina/src/app/feed/feed.component.ts
+++ escuta-as-mina/src/app/feed/feed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExemploRestService } from 'src/app/service/exemplo-rest.service';
 
 @Component({
   selector: 'app-feed',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+  post: any = null;
 
-  constructor() { }
+  constructor(public rest: ExemploRestService) { }
 
   ngOnInit() {
+    this.rest.getPost("5c9726c49ad40a141497528e")
+      .subscribe(data => {
+        this.post = data;
+        console.log(this.post);
+        
+    });
   }
-
 }
